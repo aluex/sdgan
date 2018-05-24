32978809cc5581cd067434b88248462a84321ff8
al <xaluex@gmail.com>
2018-05-24 14:47:40 +0000

    add some aux code

---
+++ train_mnist2/train.sh
@@ -0,0 +1,10 @@
+TRAIN_DIR=./train
+
+git rev-parse HEAD > ${TRAIN_DIR}/git_sha.txt
+cp train.sh ${TRAIN_DIR}
+
+python sdgan.py train ${TRAIN_DIR} \
+	--data_dir ./data/msceleb12k \
+	--data_set msceleb12k \
+	--data_id_name_tsv_fp ./data/msceleb12k/train_names.tsv
+
